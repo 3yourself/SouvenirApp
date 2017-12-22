c5deb26465dfd2ce17ea02198b97b706c0f4b26c
3yourself <3yourself@gmail.com>
2017-12-22 23:37:40 +0200

    Pushed correct PROD back URL

--- src/Config.js
+++ src/Config.js
@@ -1,1 +1,3 @@
-export const BACKEND_URL = 'http://10.0.3.2:5000/souvenir-5000d/us-central1/';
+//DEV_URL     http://10.0.3.2:5000/souvenir-5000d/us-central1/
+//PROD_URL    https://us-central1-souvenir-5000d.cloudfunctions.net/
+export const BACKEND_URL = 'https://us-central1-souvenir-5000d.cloudfunctions.net/';
